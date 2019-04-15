1cf0d394b0f0728762817ea666425cfcd1e6e79a
Herman <hermanor@kth.se>
2019-04-15 11:24:13 +0200

    Updates

---
+++ precache-manifest.5916689d57060c340ec758e1d3d0d596.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/soundydm2518/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "98ccdf598b28848ff8bc",
+    "url": "/soundydm2518/static/js/runtime~main.687a7693.js"
+  },
+  {
+    "revision": "cd011eb23e731536d154",
+    "url": "/soundydm2518/static/js/main.0fa2057f.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/soundydm2518/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "cd011eb23e731536d154",
+    "url": "/soundydm2518/static/css/main.b8aafa83.chunk.css"
+  },
+  {
+    "revision": "d8a9e94e9619ef83f67caf4af36faec5",
+    "url": "/soundydm2518/index.html"
+  }
+];
