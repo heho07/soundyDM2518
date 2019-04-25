7d8488aa26a088bb2cae8f0ec765ddec9a72f86f
emeliaj <emeliaj@kth.se>
2019-04-25 17:45:16 +0200

    Fixed navigation

--- src/App.js
+++ src/App.js
@@ -1,38 +1,19 @@
 import React, { Component } from "react";
 import CreateLogin from "./CreateLogin";
-import Start from "./Start";
+//import Start from "./Start";
 import Loading from "./Loading";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import TabContainer from "./Containers/TabContainer";
-// <<<<<<< HEAD
-//       <Router>
-//         <Route path="/" exact component={Loading} />
-//         <Route path="/login" exact component={CreateLogin} />
-//         <Route path="/start" component={Start} />
-//       </Router>
-// =======
+import Feed from "./Containers/Feed";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {/*<header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>*/}
-        <TabContainer />
-      </div>
+      <Router>
+        <Route path="/" exact component={Loading} />
+        <Route path="/login" exact component={CreateLogin} />
+        <Route path="/TabContainer" component={TabContainer} />
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-// import config from "./Config.js";
+//import config from "./Config.js";
 import * as firebase from "firebase/app";
 import "firebase/auth";
-import config from "./secrets";
+
+var config = {
+  apiKey: "AIzaSyAJZXBhqNSQG9VplYDMM-v0cADz10r16-M",
+  authDomain: "soundy-dm2518.firebaseapp.com",
+  databaseURL: "https://soundy-dm2518.firebaseio.com",
+  projectId: "soundy-dm2518",
+  storageBucket: "soundy-dm2518.appspot.com",
+  messagingSenderId: "431756069486"
+};
 
 firebase.initializeApp(config);
-
-
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
