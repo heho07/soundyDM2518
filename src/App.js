71283c8424d93c29c52ee789f920faaa9c138c7c
Herman <hermanor@kth.se>
2019-04-25 09:46:01 +0200

    tabbar finns, mall för de andra

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import TabContainer from "./Containers/TabContainer";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/*<header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +21,9 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header>*/}
+        <TabContainer/>
+
       </div>
     );
   }
---
+++ src/Containers/TabContainer.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+import Feed from "./Feed";
+
+// imports for OnsenUI
+import * as Ons from 'react-onsenui'; // Import everything and use it as 'Ons.Page', 'Ons.Button'
+import * as ons from 'onsenui'; // This needs to be imported to bootstrap the components.
+// Webpack CSS import
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
+
+class TabContainer extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      index:0,
+    }
+  }
+
+  render() {
+    return (
+      <Ons.Page>  
+        <Ons.Tabbar
+          onPreChange={({index}) => this.setState({index:index})}
+          onPostChange={() => console.log('postChange')}
+          onReactive={() => console.log('postChange')}
+          position='bottom'
+          index={this.state.index}
+          renderTabs={(activeIndex, tabbar) => [
+            {
+              content: <Ons.Page key = "About"><Feed/></Ons.Page> ,
+              tab: <Ons.Tab label="About" icon="fa-info-circle" key = "AboutTab" />
+            },
+            {
+              content: <Ons.Page key = "Yoga"><p>upload</p></Ons.Page>,
+              tab: <Ons.Tab label="Yoga" icon="fa-child" className = "testTab" key = "YogaTab"/>
+            },
+            {
+              content: <Ons.Page  key = "Breathing"><p>profile</p></Ons.Page>,
+              tab: <Ons.Tab label="Breathing" icon="fa-grin" key = "BreathingTab" />
+            }]
+          }
+        />
+      </Ons.Page>
+    );
+  }
+}
+
+export default TabContainer;
