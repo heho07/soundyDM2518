6e3cb2932443db27b633f8d4f479f23b7c196374
Cristian Osorio Bretti <cristian.osoriobretti@gmail.com>
2019-04-25 10:00:05 +0200

    Add upload of audio to firebase

--- src/AudioRecorder/AudioTest.jsx
+++ src/AudioRecorder/AudioTest.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import { ReactMic } from 'react-mic';
 
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import config from '../secrets';
+
+var app = firebase.initializeApp(config);
+
+var storage = firebase.app().storage('gs://soundy-dm2518.appspot.com/');
+var storageRef = storage.ref();
+
 export class AudioTest extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       record: false
-    }
-
+    };
   }
 
   startRecording = () => {
     this.setState({
       record: true
     });
-  }
+  };
 
   stopRecording = () => {
     this.setState({
       record: false
     });
-  }
+  };
 
   onData(recordedBlob) {
     console.log('chunk of real-time data is: ', recordedBlob);
@@ -28,6 +36,19 @@ export class AudioTest extends React.Component {
 
   onStop(recordedBlob) {
     console.log('recordedBlob is: ', recordedBlob);
+    let audioBlob = recordedBlob.blob;
+    console.log(audioBlob);
+
+    //var audioRef = storageRef.child('audios');
+    var soundRef = storageRef.child('sound');
+    soundRef
+      .put(audioBlob)
+      .then(snapshot => {
+        console.log(snapshot);
+      })
+      .catch(error => {
+        console.log('ERROR: ' + error.message);
+      });
   }
 
   render() {
@@ -39,9 +60,14 @@ export class AudioTest extends React.Component {
           onStop={this.onStop}
           onData={this.onData}
           strokeColor="#000000"
-          backgroundColor="#FF4081" />
-        <button onClick={this.startRecording} type="button">Start</button>
-        <button onClick={this.stopRecording} type="button">Stop</button>
+          backgroundColor="#FF4081"
+        />
+        <button onClick={this.startRecording} type="button">
+          Start
+        </button>
+        <button onClick={this.stopRecording} type="button">
+          Stop
+        </button>
       </div>
     );
   }
