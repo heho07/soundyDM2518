3b9b5d7ad4b28b0a34a93b0e9b60cdefe2161fca
Mikael Forsmark <mforsmar@kth.se>
2019-04-30 13:43:15 +0200

    WIP: Can get the url of the file in the firebase bucket, needs to extract the actual value

--- src/AudioRecorder/AudioTest.jsx
+++ src/AudioRecorder/AudioTest.jsx
@@ -10,8 +10,6 @@ var app = firebase.initializeApp(config);
 var storage = firebase.app().storage('gs://soundy-dm2518.appspot.com/');
 var storageRef = storage.ref();
 
-var blobURL;
-
 export class AudioTest extends React.Component {
   constructor(props) {
     super(props);
@@ -19,7 +17,8 @@ export class AudioTest extends React.Component {
       record: false,
       blobObject: null,
       isRecording: false,
-      isPaused: false
+      isPaused: false,
+      audioURL: ""
     };
     this.onStop = this.onStop.bind(this);
   }
@@ -44,17 +43,26 @@ export class AudioTest extends React.Component {
     let audioBlob = recordedBlob.blob;
     this.setState({ blobURL : recordedBlob.blobURL });
     //var audioRef = storageRef.child('audios');
+
+    //TODO: Random audio name generation
     var soundRef = storageRef.child('sound');
     soundRef
       .put(audioBlob)
       .then(snapshot => {
         console.log(snapshot);
+        this.audioURL = soundRef.getDownloadURL().then(function(downloadURL) {
+          //console.log('File available at', downloadURL);
+          // TODO: I can get the url of the file in the bucket inside here but not outside, then it's just a promise.
+          return downloadURL;
+        });
+        console.log(this.audioURL);
       })
       .catch(error => {
         console.log('ERROR: ' + error.message);
       });
   }
 
+  // If you want background image to <video>: style={{backgroundImage: 'url(' + "https://picsum.photos/400/600" + ')'}}
   render() {
     const { blobURL, isRecording, isPaused } = this.state;
     return (
