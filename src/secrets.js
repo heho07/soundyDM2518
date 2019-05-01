218e6a11e85db46bc034d1c9d7dba33b82ba941d
emeliajohansson <45037580+emeliajohansson@users.noreply.github.com>
2019-05-01 17:49:07 +0200

    Update secrets.js

--- src/secrets.js
+++ src/secrets.js
@@ -1,10 +1,1 @@
-const config = {
-  apiKey: 'AIzaSyAJZXBhqNSQG9VplYDMM-v0cADz10r16-M',
-  authDomain: 'soundy-dm2518.firebaseapp.com',
-  databaseURL: 'https://soundy-dm2518.firebaseio.com',
-  projectId: 'soundy-dm2518',
-  storageBucket: 'soundy-dm2518.appspot.com',
-  messagingSenderId: '431756069486'
-};
 
-export default config;
