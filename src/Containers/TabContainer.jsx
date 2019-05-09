b62a9f99c0060f57b473940e3f41c26a5ba2ff72
Cristian Osorio Bretti <cristian.osoriobretti@gmail.com>
2019-05-09 14:52:52 +0200

    Fix leaky listeners #3

--- src/Containers/TabContainer.jsx
+++ src/Containers/TabContainer.jsx
@@ -93,15 +93,7 @@ class TabContainer extends Component {
   }
 
   async componentDidMount() {
-    // ifall man skulle bli utloggad (tror jag /H)
     redirectWhenOAuthChanges(this.props.history);
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        this.setState({ currentUser: user });
-      } else {
-        this.setState({ currentUser: null });
-      }
-    });
 
     // kopplar upp till databasen
     var storage = firebase.app().storage("gs://soundy-dm2518.appspot.com/");
