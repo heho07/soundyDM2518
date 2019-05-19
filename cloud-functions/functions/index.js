870c18072529b0e4cf8d2247b78ed90155a3ca01
Cristian Osorio Bretti <cristian.osoriobretti@gmail.com>
2019-05-19 20:06:53 +0200

    Remove sound doc #22

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions');
 // The Firebase Admin SDK
 const admin = require('firebase-admin');
 admin.initializeApp();
+const db = admin.firestore();
 
 exports.getAllUsers = functions.https.onCall((data, context) => {
     return admin.auth().listUsers().then(userRecords => {
@@ -15,8 +16,19 @@ exports.removeUser = functions.https.onCall((data, context) => {
     const {uid} = data 
     return admin.auth().deleteUser(uid)
     .then(() => {
-        //TODO: remove all user stuff
-        return {completed: true}
+        let postsFromUser = db.collection("all-sounds").where("user", "==", uid)
+        return postsFromUser.get()
+        .then(querySnapshot => {
+            var listOfSounds = []
+            querySnapshot.forEach(doc => {
+                listOfSounds.push(doc.data().url)
+                doc.ref.delete()
+            })
+            listOfSounds.forEach(url => {
+                //TODO: Delete actual sound from storage
+            })
+            return {completed: true, sounds: listOfSounds}
+        })
     })
     .catch(() => ({completed: false}))
 });
