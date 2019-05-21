3174ce5082287665dcdf00ea6afa91f7dceb39a5
Cristian Osorio Bretti <cristian.osoriobretti@gmail.com>
2019-05-21 15:48:13 +0200

    Add logic for deleting specific post #22

--- src/Containers/ShowUsersPosts.jsx
+++ src/Containers/ShowUsersPosts.jsx
@@ -2,10 +2,11 @@ import React, { Component } from "react";
 
 import * as firebase from "firebase/app";
 import "firebase/firestore";
+import "firebase/functions";
 
 // imports for OnsenUI
 import * as Ons from "react-onsenui"; // Import everything and use it as 'Ons.Page', 'Ons.Button'
-//import * as ons from "onsenui"; // This needs to be imported to bootstrap the components.
+import * as ons from "onsenui"; // This needs to be imported to bootstrap the components.
 // Webpack CSS import
 import "onsenui/css/onsenui.css";
 import "onsenui/css/onsen-css-components.css";
@@ -18,6 +19,7 @@ class ShowUsersPosts extends Component {
       hasFetched: false,
       listOfPosts: []
     };
+    this.removeOnePost = firebase.functions().httpsCallable("removeOnePost");
   }
 
   componentDidMount() {
@@ -31,6 +33,7 @@ class ShowUsersPosts extends Component {
       if (nextProps.user) {
         this.getUsersPost(nextProps.user);
       }
+      return true
     }
   }
 
@@ -39,24 +42,44 @@ class ShowUsersPosts extends Component {
     let postsFromUser = this.db
       .collection("all-sounds")
       .where("user", "==", uid);
-    return postsFromUser.get().then(querySnapshot => {
+    postsFromUser.get().then(querySnapshot => {
       let posts = [];
       querySnapshot.forEach(doc => {
         let documentData = doc.data();
-        console.log(documentData);
+        documentData.id = doc.id;
         posts.push(documentData);
 
         //Lägg till de promise man får i en lista
       });
-      console.log();
       this.setState({ listOfPosts: posts, hasFetched: true });
     });
   };
 
-  renderListItems() {
-    console.log("renderListItems");
+  showDeletePostConfirmation = (post, index) => {
+    ons.notification.confirm('Are you sure you want to delete this post?')
+    .then(selected => {
+      //Selected is 1 for OK and 0 for Cancel
+      if(selected){
+        this.deletePost(post, index)
+      }
+    })
+  }
 
-    return this.state.listOfPosts.map(post => (
+  deletePost = (post, index) => {
+    //Delete post
+    post.showSpinnerOnButton = true
+    this.state.listOfPosts[index].showSpinnerOnButton = true;
+    this.forceUpdate()
+    const {id, storageUri} = post
+    this.removeOnePost({id, storageUri}).then(result => {
+      if(result.data.completed){
+        this.setState({hasFetched: false})
+      }
+    })
+  }
+
+  renderListItems() {
+    return this.state.listOfPosts.map((post, index) => (
       <Ons.ListItem key={post.time}>
         <div className="left">
           <img src={post.imgUrl} className="list-item__thumbnail" />
@@ -65,8 +88,12 @@ class ShowUsersPosts extends Component {
 
         {this.props.shouldShowDeleteButton && (
           <div className="right">
-            <Ons.Fab onClick={this.upload} className="saveButton">
-              <Ons.Icon icon="trash" />
+            <Ons.Fab
+              onClick={() => this.showDeletePostConfirmation(post, index)}
+              className="saveButton">
+              <Ons.Icon
+                spin={!!post.showSpinnerOnButton}
+                icon={post.showSpinnerOnButton ? "spinner" : "trash"} />
             </Ons.Fab>
           </div>
         )}
